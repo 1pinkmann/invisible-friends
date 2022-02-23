d7b27c4cfa34bda0f09590127b0fefbf34ac1f3c
manuochoa <manu_8a@outlook.com>
2022-02-24 01:13:41 +0200

    wallet connect

--- src/App.js
+++ src/App.js
@@ -7,19 +7,33 @@ import Desc from "./components/Desc";
 import Footer from "./components/Footer";
 import { ethers } from "ethers";
 import Quantity from "./components/common/Quantity";
+import ConnectPopup from "./components/ConnectPopup";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Web3 from "web3";
 
 const NODE_URL = "https://rinkeby-light.eth.linkpool.io/";
 const nftAddress = "0xD2B7D5acCD7EAA79b54967dff9247D791aB300cc";
 // const nftAddress = "0x66A736175224240E9754dfd1C69A471ce3702Cb8";
+let ABI = [
+  {
+    inputs: [{ internalType: "uint256", name: "_amount", type: "uint256" }],
+    name: "publicSaleMint",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+];
 
 export default function App() {
   const [userAddress, setUserAddress] = useState("");
   const [userNFTs, setUserNFTs] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [walletType, setWalletType] = useState("");
+  const [popupShowed, setPopupShowed] = useState(false);
   const [value, setValue] = useState(1);
   const [minted, setMinted] = useState("1468");
 
-  const connectWallet = async () => {
+  const connectMetamask = async () => {
     console.log("hola");
     try {
       const accounts = await window.ethereum.request({
@@ -49,6 +63,34 @@ export default function App() {
       window.ethereum.on("chainChanged", (_chainId) =>
         window.location.reload()
       );
+      setPopupShowed(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWalletConnect = async () => {
+    try {
+      console.log("hola");
+      const provider = new WalletConnectProvider({
+        rpc: {
+          1: NODE_URL,
+
+          // 97: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+        },
+        chainId: 1,
+        infuraId: null,
+      });
+
+      await provider.enable();
+      const web3 = new Web3(provider);
+
+      // const accounts = await ethers.listAccounts();
+      const accounts = await web3.eth.getAccounts();
+
+      setUserAddress(accounts[0]);
+      setWalletType("WALLET_CONNECT");
+      setPopupShowed(false);
     } catch (error) {
       console.log(error);
     }
@@ -70,24 +112,45 @@ export default function App() {
   const handleMint = async () => {
     setIsLoading(true);
     try {
-      let provider = new ethers.providers.Web3Provider(window.ethereum);
-      let signer = provider.getSigner(0);
-
-      let contractInstance = new ethers.Contract(
-        nftAddress,
-        ["function publicSaleMint(uint256 _amount) public payable"],
-        signer
-      );
-
+      let provider;
+      let signer;
+      let contractInstance;
       let price = (value * 0.25 * 10 ** 18).toString();
+      let receipt;
 
-      let tx = await contractInstance.publicSaleMint(value, {
-        value: price,
-      });
+      if (walletType === "WALLET_CONNECT") {
+        provider = new WalletConnectProvider({
+          rpc: {
+            1: NODE_URL,
+          },
+        });
 
-      let receipt = await tx.wait();
+        await provider.enable();
+        let web3 = new Web3(provider);
+
+        contractInstance = new web3.eth.Contract(ABI, nftAddress);
+
+        receipt = await contractInstance.methods
+          .publicSaleMint(value)
+          .send({ from: userAddress, value: price });
+      } else {
+        provider = new ethers.providers.Web3Provider(window.ethereum);
+        signer = provider.getSigner(0);
+
+        contractInstance = new ethers.Contract(
+          nftAddress,
+          ["function publicSaleMint(uint256 _amount) public payable"],
+          signer
+        );
+        let tx = await contractInstance.publicSaleMint(value, {
+          value: price,
+        });
+        receipt = await tx.wait();
+      }
+
       if (receipt) {
         console.log(receipt);
+        setMinted(minted + 1);
         // checkMinted();
       }
     } catch (error) {
@@ -129,7 +192,10 @@ export default function App() {
               </button>
             </div>
           ) : (
-            <button onClick={connectWallet} className="button banner__button">
+            <button
+              onClick={() => setPopupShowed(true)}
+              className="button banner__button"
+            >
               Connect wallet
             </button>
           )}
@@ -151,6 +217,13 @@ export default function App() {
         <Desc />
       </main>
       <Footer />
+      <ConnectPopup
+        connectMetamask={connectMetamask}
+        connectWalletConnect={connectWalletConnect}
+        popupShowed={popupShowed}
+        setPopupShowed={setPopupShowed}
+        className="popup--connect"
+      />
     </>
   );
 }
